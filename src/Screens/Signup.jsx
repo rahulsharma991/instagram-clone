b454b7af3ddb5b61664d5af680f54cc56fe071df
narender singh <narendersingh@narenders-MacBook-Pro.local>
2021-04-03 00:48:36 +0530

    first commit

---
+++ src/Screens/Signup.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text, Input, Button } from 'react-native';
+
+export default function Signup({
+
+}) {
+    return (
+        <View>
+            <Text>Choose Your Username</Text>
+            <Text>You can always change it later</Text>
+            <Input 
+
+            />
+            <Button></Button>
+        </View>
+    )
+}
